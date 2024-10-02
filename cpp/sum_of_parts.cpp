20ccfbbb08e37b877518a8ec68ceb6da6ec355ce
rachit74 <rachithooda09@gmail.com>
2024-10-02 12:05:19 +0530

    solve sum of parts

---
+++ cpp/sum_of_parts.cpp
@@ -0,0 +1,55 @@
+/*
+Let us consider this example (array written in general format):
+
+ls = [0, 1, 3, 6, 10]
+
+Its following parts:
+
+ls = [0, 1, 3, 6, 10]
+ls = [1, 3, 6, 10]
+ls = [3, 6, 10]
+ls = [6, 10]
+ls = [10]
+ls = []
+
+The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+Other Examples:
+
+ls = [1, 2, 3, 4, 5, 6] 
+parts_sums(ls) -> [21, 20, 18, 15, 11, 6, 0]
+
+ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]
+parts_sums(ls) -> [10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]
+
+*/
+
+#include <iostream>
+#include <vector>
+
+std::vector<unsigned long long> partsSum(const std::vector<unsigned long long>& ls){
+    std::vector<unsigned long long> sum_arr;
+    unsigned long long total_sum = 0;
+
+    // calculate total sum
+    for (unsigned long long num: ls) {
+        total_sum += num;
+    }
+    sum_arr.push_back(total_sum);
+
+
+    // minus each element from total sum and push back to array
+    for (unsigned long long num: ls) {
+        total_sum -= num;
+        sum_arr.push_back(total_sum);
+    }
+
+   
+
+    return sum_arr;
+}
+
+int main() {
+    return 0;
+}
